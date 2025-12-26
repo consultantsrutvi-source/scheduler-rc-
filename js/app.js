2509f2d9d00f93776259b0a56d7c461f58931477
consultantsrutvi-source <consultantsrutvi@gmail.com>
2025-12-26 10:23:57 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,11 +3,13 @@ const MASTER_PIN = "3011";
 
 // ===== LOGIN =====
 function login() {
-  const pin = document.getElementById("pin").value.trim();
+  const inputs = document.querySelectorAll(".pin-input");
   const error = document.getElementById("error");
+  let pin = "";
+
+  inputs.forEach(input => pin += input.value);
 
   if (pin === MASTER_PIN) {
-    // temporary success action
     document.body.innerHTML = `
       <div style="
         height:100vh;
@@ -27,6 +29,22 @@ function login() {
     error.innerText = "Invalid PIN";
   }
 }
+const pinInputs = document.querySelectorAll(".pin-input");
+
+pinInputs.forEach((input, index) => {
+  input.addEventListener("input", () => {
+    if (input.value && index < pinInputs.length - 1) {
+      pinInputs[index + 1].focus();
+    }
+  });
+
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace" && !input.value && index > 0) {
+      pinInputs[index - 1].focus();
+    }
+  });
+});
+
 
 // ===== ENTER KEY SUPPORT =====
 document.addEventListener("keydown", function (e) {
